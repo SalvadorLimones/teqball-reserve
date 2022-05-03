77337d5c40ba59fdad896d798da6228721769d52
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-05-03 13:29:44 +0200

    sémák

---
+++ backend/model/group.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const memberSchema = Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  role: {
+    type: String
+  }
+})
+
+const participantSchema = Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  message: {
+    type: String
+  }
+})
+
+const eventSchema = mongoose.Schema({
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  place_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'place'
+  },
+  date: {
+    type: Date,
+  },
+  participants: {
+    type: [participantSchema]
+  }
+});
+
+const groupSchema = Schema({
+  name: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  members: {
+    type: [memberSchema]
+  },
+  events: {
+    type: [eventSchema]
+  }
+})
+
+const Member = mongoose.model("Member", memberSchema);
+const Participant = mongoose.model("Participant", participantSchema);
+const Event = mongoose.model("Event", eventSchema);
+const Group = mongoose.model("Group", groupSchema);
+
+module.exports = { Member: Member, Participant: Participant, Event: Event, Group: Group }
