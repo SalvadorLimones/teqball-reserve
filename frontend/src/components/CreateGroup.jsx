7b45ef2c0a2b2ad31d904bbbe772ee0bbdc65d31
agolautner <lautner.agoston@gmail.com>
2022-05-06 01:15:33 +0200

    ultrataho commit

--- frontend/src/components/CreateGroup.jsx
+++ frontend/src/components/CreateGroup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { createGroup } from '../api/groupActions';
+import {useNavigate} from "react-router-dom"
 
 const CreateGroup = () => {
 
@@ -7,13 +8,23 @@ const CreateGroup = () => {
     const [groupDescription, setGroupDescription] = useState('');
     const [message, setMessage] = useState('Please set a name and a description for your new group!');
 
+    const navigate = useNavigate();
+
+    const handleCreateGroup = () => {
+      createGroup(groupName, groupDescription, setMessage);
+      navigate('/');
+      setTimeout(() => {
+        navigate('/groups');
+      }, 100)
+    }
+
   return (
         <div className='CreateGroup'>
             <h2>Create a new group</h2>
             <p>{message}</p>
             <input value={groupName} onChange={(e) => setGroupName(e.target.value)} placeholder="Choose a name for your group!"/>
             <textarea name="description" id="description" cols="30" rows="10" onChange={(e) => setGroupDescription(e.target.value)} placeholder="Describe your group!"></textarea>
-            <button onClick={() => createGroup(groupName, groupDescription, setMessage)}>CREATE GROUP</button>
+            <button onClick={() => handleCreateGroup()}>CREATE GROUP</button>
         </div>
   )
 }
