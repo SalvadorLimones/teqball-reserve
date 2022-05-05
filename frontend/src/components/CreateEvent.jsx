afc058125970ce1ffba8cde63789b9968dcbac2b
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-05-05 18:50:38 +0200

    add createevent component

---
+++ frontend/src/components/CreateEvent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateEvent = () => {
+  return <button>CreateEvent</button>;
+};
+
+export default CreateEvent;
--- frontend/src/components/GroupDetails.jsx
+++ frontend/src/components/GroupDetails.jsx
@@ -1,59 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import { getMembers, acceptUser, refuseUser, joinGroup, leaveGroup } from '../api/groupActions';
+import React, { useEffect, useState } from "react";
+import {
+  getMembers,
+  acceptUser,
+  refuseUser,
+  joinGroup,
+  leaveGroup,
+} from "../api/groupActions";
+import Events from "./Events";
 
-const GroupDetails = ({group, reload}) => {
-    const [members, setMembers] = useState([]);
+const GroupDetails = ({ group, reload }) => {
+  const [members, setMembers] = useState([]);
 
-    const handleGetMembers = async (id) => {
-        setMembers(await getMembers(id));
-    }
+  const handleGetMembers = async (id) => {
+    setMembers(await getMembers(id));
+  };
 
-    const handleAccept = (memberId) => {
-        console.log('accepting join request');
-        acceptUser(group.id, memberId);
-        // handleGetMembers(group.id);
-    }
+  const handleAccept = (memberId) => {
+    console.log("accepting join request");
+    acceptUser(group.id, memberId);
+    // handleGetMembers(group.id);
+  };
 
-    const handleReject = (memberId) => {
-        console.log('rejecting join request');
-        refuseUser(group.id, memberId);
-        // handleGetMembers(group.id);
-    }
+  const handleReject = (memberId) => {
+    console.log("rejecting join request");
+    refuseUser(group.id, memberId);
+    // handleGetMembers(group.id);
+  };
 
-    const handleJoin = async () => {
-        console.log('handlejoin has been called')
-        await joinGroup(group.id);
-        reload();
-    }
+  const handleJoin = async () => {
+    console.log("handlejoin has been called");
+    await joinGroup(group.id);
+    reload();
+  };
 
-    const handleLeave = async () => {
-        console.log('handleleave has been called');
-        await leaveGroup(group.id);
-        reload();
-    }
+  const handleLeave = async () => {
+    console.log("handleleave has been called");
+    await leaveGroup(group.id);
+    reload();
+  };
 
-    useEffect(() => {
-        console.log(group)
-        if (group.status !== 'stranger') handleGetMembers(group.id)
-        console.log(members)
-    }, [])
+  useEffect(() => {
+    console.log(group);
+    if (group.status !== "stranger") handleGetMembers(group.id);
+    console.log(members);
+  }, []);
 
   return (
-    <div className='GroupDetails'>
-        <h2>{group.name}</h2>
-        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, modi ex, harum ipsam alias sunt tenetur temporibus, voluptas fuga in porro? Ab quis, exercitationem ad maxime quas incidunt optio assumenda?</h4>
-        <p>Your role in this group: {group.status}</p>
-        {group.status === 'stranger' && <button onClick={() => handleJoin()}>JOIN</button>}
-        {
-            (group.status === 'member' || group.status === 'admin' || group.status === 'pending') && <button onClick={() => handleLeave()}>LEAVE</button>
-        }
-        {((group.status === 'owner') || (group.status === 'admin') || (group.status === 'member')) && 
-        <div className='GroupDetails-members'>
-            <h5>Members</h5>
-            {members && members.map((m, i) => <div key={i}>{m.member_name} {(m.role === 'pending' && ((group.status === 'owner') || (group.status === 'admin'))) && <div><button onClick={() => handleAccept(m.member_id)}>accept</button><button onClick={() => handleReject(m.member_id)}>reject</button></div>}</div>)}
-        </div>}
+    <div className="GroupDetails">
+      <h2>{group.name}</h2>
+      <h4>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, modi
+        ex, harum ipsam alias sunt tenetur temporibus, voluptas fuga in porro?
+        Ab quis, exercitationem ad maxime quas incidunt optio assumenda?
+      </h4>
+      <p>Your role in this group: {group.status}</p>
+      {group.status === "stranger" && (
+        <button onClick={() => handleJoin()}>JOIN</button>
+      )}
+      {(group.status === "member" ||
+        group.status === "admin" ||
+        group.status === "pending") && (
+        <button onClick={() => handleLeave()}>LEAVE</button>
+      )}
+      {(group.status === "owner" ||
+        group.status === "admin" ||
+        group.status === "member") && (
+        <div className="GroupDetails-members">
+          <h5>Members</h5>
+          {members &&
+            members.map((m, i) => (
+              <div key={i}>
+                {m.member_name}{" "}
+                {m.role === "pending" &&
+                  (group.status === "owner" || group.status === "admin") && (
+                    <div>
+                      <button onClick={() => handleAccept(m.member_id)}>
+                        accept
+                      </button>
+                      <button onClick={() => handleReject(m.member_id)}>
+                        reject
+                      </button>
+                    </div>
+                  )}
+              </div>
+            ))}
+        </div>
+      )}
+      <div>
+        <Events />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GroupDetails
+export default GroupDetails;
