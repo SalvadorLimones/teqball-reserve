1a2ccd421efd230245829825741aa59581bdbfd9
Cserne <angeli.cserne@gmail.com>
2022-05-05 18:27:50 +0200

    Events list.

---
+++ frontend/src/api/events.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+export const listEvents = async (userId, groupId, eventId) => {
+    const token = localStorage.getItem("token");
+    if (!token) window.alert("Please log in first");
+    if (token && userId && groupId && eventId) {
+        const requestBody = { token: token, groupId: groupId, userId: userId, eventId: eventId };
+
+        try { 
+            const response = await axios.get("http://localhost:5000/api/group/list", {
+                requestBody
+              })
+            return response;
+        } catch (error) {
+            console.log('something has gone wrong! ', error);
+            return error;
+        }
+    }
+};
---
+++ frontend/src/components/Events.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { listEvents } from '../api/events';
+
+const Events = () => {
+  return (
+    <div>Events</div>
+  )
+}
+
+export default Events
