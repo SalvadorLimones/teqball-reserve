b0688567bf9d48362c163b91bbaf394eb1e8d234
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-05-05 20:14:09 +0200

    get post

--- frontend/src/api/events.js
+++ frontend/src/api/events.js
@@ -28,13 +28,16 @@ export const addEvent = async (group_id, name, venue, date) => {
   if (!token) window.alert("Please log in first");
   if ((token && group_id && name && venue, date)) {
     try {
-      const response = await axios.get("http://localhost:5000/api/event/list", {
-        token: token,
-        group_id: group_id,
-        name: name,
-        venue: venue,
-        date: date,
-      });
+      const response = await axios.post(
+        "http://localhost:5000/api/event/register",
+        {
+          token: token,
+          group_id: group_id,
+          name: name,
+          venue: venue,
+          date: date,
+        }
+      );
       return response;
     } catch (error) {
       console.log("something has gone wrong! ", error);
